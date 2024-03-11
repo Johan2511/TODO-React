912861db106586aec728be62f95610fa06c4a7a7
Johan2511 <johanpenuelah@gmail.com>
2024-03-11 12:10:11 -0500

    composicion de componentes sin react context

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import { AppUI } from './AppUI';
-import { TodoProvider } from '../TodoContext';
+import { useTodos } from './useTodos';
+import TodoHeader from '../Todoheader';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { TodosLoading } from '../TodosLoading';
+import { TodosError } from '../TodosError';
+import { EmptyTodos } from '../EmptyTodos';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { TodoForm } from '../TodoForm'; 
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { Modal } from '../Modal';
 
 // const defaultTodos = [
 //   { text: 'cortar cebolla' , completed: true},
@@ -17,11 +27,69 @@ import { TodoProvider } from '../TodoContext';
 
 function App() {
 
+  const {
+    
+    loading,
+    error,
+    completedTodos,
+    totalTodos,
+    searchValue,
+    setSearchValue,
+    serchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    addTodo
+  } = useTodos();
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
-  )
+    <>
+      <TodoHeader >
+      
+        <TodoCounter 
+        totalTodos={totalTodos}
+        completedTodos={completedTodos}/>
+
+        <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}/>  
+      
+      </TodoHeader>
+
+          <TodoList>
+            {loading  && (
+              <>
+            <TodosLoading />
+            <TodosLoading />
+            <TodosLoading />
+            </>
+            )}
+            {error  && <TodosError/>}
+            {(!loading && serchedTodos.length === 0) && <EmptyTodos/>}
+
+            {serchedTodos.map(todo => (
+              <TodoItem 
+              key={todo.text} 
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)} 
+              onDelete={() => deleteTodo(todo.text)} />
+            ))}
+        </TodoList>
+
+      <CreateTodoButton
+        setOpenModal={setOpenModal} />
+
+      {openModal && (
+        <Modal>
+        <TodoForm 
+        addTodo={addTodo}
+        setOpenModal={setOpenModal} />
+      </Modal>
+      )}
+    </>
+  );
 }
 
 
--- src/App/useTodos.js
+++ src/App/useTodos.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
-const TodoContext = React.createContext();
 
-function TodoProvider({children}) {
+function useTodos() {
 
     const {
         item: todos,
@@ -54,8 +53,7 @@ function TodoProvider({children}) {
         }
 
 
-    return (
-        <TodoContext.Provider value={{
+    return {
             loading,
             error,
             completedTodos,
@@ -68,12 +66,9 @@ function TodoProvider({children}) {
             deleteTodo,
             openModal,
             setOpenModal,
-        }}>
-            {children}
-        </TodoContext.Provider>
-    );    
+    }  
 }
 
 
 
-export { TodoContext, TodoProvider };
+export { useTodos };
