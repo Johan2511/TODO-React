bdbaaa4c7f327bda2fe91d69b7ff4ca9871f7936
Johan2511 <johanpenuelah@gmail.com>
2024-03-13 15:52:10 -0500

    reutilizacion de modal para mostrar el mensaje de cambio de estado em localStorage ademas de cambios en el estilo yen el Componente ChangeAlert

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,18 +1,27 @@
 import  React from 'react'
 import { withStorageListener } from './withStorageListener'
+import { Modal } from '../Modal'
+import { Load } from '../Icons/Load'
+import './ChangeAlert.css'
 
 function ChangeAlert({show, toggleShow}) {
 
-    console.log('toggleShow:', toggleShow);
+    // console.log('toggleShow:', toggleShow);
     if (show) {
         return (
-            <div>
-                <p>Hubo cambios</p>
-                <button
-                onClick={toggleShow}>
-                    Volver a cargar la información
-                </button>
+            <Modal >
+            <div className='ModalBackground'>
+                <div className='ModalContent'>
+                    <Load width={24} height={24} />
+                    <p><span>¡Actualización de tareas!</span></p>
+                    <p>Por favor, actualiza la página</p>
+                    <button
+                    onClick={toggleShow}>
+                        Volver a cargar la información
+                    </button>
+                </div>
             </div>
+            </Modal>
         )
     } else {
         return null  
